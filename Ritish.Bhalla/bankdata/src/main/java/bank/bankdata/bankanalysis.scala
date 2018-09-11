4f9b3e4e4afeda08a479ca18b72c3bf7d0cfdad6
ritishbhalla01 <40321041+ritishbhalla01@users.noreply.github.com>
2018-09-11 11:09:01 +0530

    Update bankanalysis.scala

--- Ritish.Bhalla/bankdata/src/main/java/bank/bankdata/bankanalysis.scala
+++ Ritish.Bhalla/bankdata/src/main/java/bank/bankdata/bankanalysis.scala
@@ -1,8 +1,9 @@
+
 package bank.bankdata
 import org.apache.spark._
 object bankanalysis{
 def main(arg: Array[String]): Unit = {
-var sparkConf = new SparkConf()
+var sparkConf = new SparkConf().setMaster("local").setAppName("Transaction")
 var sc = new SparkContext(sparkConf)
 var firstRdd = sc.textFile("file:///home/ritish/Downloads/Transaction Sample data-1.csv")
 var secondRdd = sc.textFile("file:///home/ritish/Downloads/Transaction Sample data-2.csv")
@@ -24,7 +25,8 @@ var finalCreditsum2 = pairRddCredit2.map{case (key,value) => (key,value.sum)}
 var zipRddCredit2 = finalCreditsize2.zip(finalCreditsum2).map{case ((a,b),(c,d)) => (a,d/b)}
 //Combining C from first and second file
 var zippedCredit = zipRddCredit.zip(zipRddCredit2).map{case ((a,b),(c,d)) => (a,(b+d)/2)}
-var averageof_CreditCard = zippedCredit.map{case (a,b) => (f"Average Transaction Amount from Credit = $b%.2f")}
+//var averageof_CreditCard = zippedCredit.map{case (a,b) => (f"Average Transaction Amount from Credit = $b%.2f")}.foreach(println)
+
 // for filtering D from first file
 var filterDebit = bankdata.filter(x => x.card == 'D')
 var pairRddDebit = filterDebit.map(x => (x.card,x.amount)).groupByKey().collect()
@@ -39,7 +41,8 @@ var finalDebitsum2 = pairRddDebit2.map{case (key,value) => (key,value.sum)}
 var zipRddDebit2 = finalDebitsize2.zip(finalDebitsum2).map{case ((a,b),(c,d)) => (a,d/b)}
 //Combining D from first and second file
 var zippedDebit = zipRddDebit.zip(zipRddDebit2).map{case ((a,b),(c,d)) => (a,(b+d)/2)}
-var averageof_DebitCard = zippedDebit.map{case (a,b) => (f"Average Transaction Amount from Debit = $b%.2f")}
+//var averageof_DebitCard = zippedDebit.map{case (a,b) => (f"Average Transaction Amount from Debit = $b%.2f")}.foreach(println)
+
 
 var groupYear = filterCredit.map(x => (x.year,x.amount)).groupByKey().collect()
 var finalyearsize  = groupYear.map{case (key,value) => (key,value.size)} 
@@ -50,7 +53,8 @@ var finalyearsize2  = groupYear2.map{case (key,value) => (key,value.size)}
 var finalyearsum2 = groupYear2.map{case (key,value) => (key,value.sum)}
 var zip2 = finalyearsize2.zip(finalyearsum2).map{case ((a,b),(c,d)) =>(a,d/b)}
 var yearCredit = zip1.zip(zip2).map{case ((a,b),(c,d)) => (a,(b+d)/2)}
-var averageof_allyear = yearCredit.map{case (a,b) =>(f"Year = $a%5s",f"Average Transaction Amount from Credit =$b%.2f")}
+//var averageof_allyear = yearCredit.map{case (a,b) =>(f"Year = $a%5s",f"Average Transaction Amount from Credit =$b%.2f")}.foreach(println)
+
 
 var groupYear_debit = filterDebit.map(x => (x.year,x.amount)).groupByKey().collect()
 var finalyearsize_debit  = groupYear_debit.map{case (key,value) => (key,value.size)} 
@@ -61,7 +65,8 @@ var finalyearsize2_debit  = groupYear2_debit.map{case (key,value) => (key,value.
 var finalyearsum2_debit = groupYear2_debit.map{case (key,value) => (key,value.sum)}
 var zip2_debit = finalyearsize2_debit.zip(finalyearsum2_debit).map{case ((a,b),(c,d)) =>(a,d/b)}
 var yearCredit_debit = zip1_debit.zip(zip2_debit).map{case ((a,b),(c,d)) => (a,(b+d)/2)}
-var averageof_allyear_debit = yearCredit_debit.map{case (a,b) =>(f"Year = $a%5s",f"Average Transaction Amount from Debit =$b%.2f")}
+//var averageof_allyear_debit = yearCredit_debit.map{case (a,b) =>(f"Year = $a%5s",f"Average Transaction Amount from Debit =$b%.2f")}
+
 
 var credit1rdd = bankdata.map(x => (x.account_id,x)).groupByKey().map{case (a,b) => (a,b.filter(_.card == 'C').map(_.amount))}
 var sum_C1 = credit1rdd.map(x => (x._1,x._2.sum))
@@ -82,6 +87,25 @@ var maxKey2 = balance.max()(new Ordering[Tuple2[Int, Float]]() {
   override def compare(x: (Int, Float), y: (Int, Float)): Int = 
       Ordering[Float].compare(x._2, y._2)
 })
+
+
+//var abc = maxKey2.map{case (a,b) => (f"Account id = $a%10s",f" Balance = $b%5s")}.foreach(println)
+println("")
+var averageof_CreditCard = zippedCredit.map{case (a,b) => (f" Average Transaction Amount from Credit = $b%.2f")}.foreach(println)
+var averageof_DebitCard = zippedDebit.map{case (a,b) => (f" Average Transaction Amount from Debit = $b%.2f")}.foreach(println)
+println("")
+var averageof_allyear = yearCredit.map{case (a,b) =>(f"Year = $a%5s",f" Average Transaction Amount from Credit = $b%.2f")}.foreach(println)
+println("")
+var averageof_allyear_debit = yearCredit_debit.map{case (a,b) =>(f"Year = $a%5s",f" Average Transaction Amount from Debit = $b%.2f")}.foreach(println)
+println("")
+println("Account_id whose balance is highest and its Balance = "+maxKey2)
+println("")
+/*println(averageof_CreditCard)
+println(averageof_DebitCard)
+println(averageof_allyear)
+println(averageof_allyear_debit)
+println(balanceRdd)*/
+sc.stop()
 }
 case class Bank(account_id: Int, name: String, year: String, card: Char, amount: Float) 
 def parse(row: String): Bank= {
